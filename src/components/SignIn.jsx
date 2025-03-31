915d09f7c7468e8928590be106f33474d2a14ef8
Md Nazmul Islam <nazmulislam5691@gmail.com>
2025-03-31 20:59:22 +0600

    complete authentication with google

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,18 +1,66 @@
-import { GoogleAuthProvider } from "firebase/auth/web-extension";
-
+import { GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+import auth from "../firebase/firebase.init";
+import { useState } from "react";
 
 const SignIn = () => {
 
-    const provider = new GoogleAuthProvider()
+
+    const [user, setUser] = useState(null);
+
+    const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider()
 
 
     const handleGoogleSignIn = () =>{
-        console.log("google coming soon");
+        signInWithPopup(auth, googleProvider)
+        .then(result =>{
+            console.log(result);
+            setUser(result.user)
+        })
+        .catch(error =>{
+            console.log(error);
+        })
     }
+
+    const handleGithubSignIn = () =>{
+        signInWithPopup(auth, githubProvider)
+        .then(result =>{
+            console.log(result.user);
+            setUser(result.user)
+        })
+        .catch(error => console.log(error))
+    }
+
+    const handleSignOut = () =>{
+        signOut(auth)
+        .then(() =>{
+            console.log('user sign out');
+            setUser(null);
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+    }
+
+
 
     return (
         <div>
-            <button onClick={handleGoogleSignIn}>sign in with google</button>
+            {
+                user ? <button className="bg-blue-500 text-black p-2 rounded-md" onClick={handleSignOut}>Sign Out</button> : 
+                <>
+                    <button className="bg-blue-500 text-black p-2 rounded-md mr-4" onClick={handleGoogleSignIn}>sign in with google</button>
+                    <button className="bg-blue-500 text-black p-2 rounded-md" onClick={handleGithubSignIn}>sign in with github</button>
+                </>
+            }
+
+            {
+                user && <div>
+                    <h2>{user.displayName}</h2>
+                    <h2>{user.email}</h2>
+                </div>
+            }
+           
         </div>
     );
 };
